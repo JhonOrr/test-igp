cf10a63eeb60606cf34ef4ea2c7591c574d42e43
Jhon Orrego <jorregoj@outlook.com>
2024-04-17 05:47:44 -0500

    new commit

--- app/geovisor/page.js
+++ app/geovisor/page.js
@@ -1,5 +1,30 @@
-export default function Geovisor(){
-    return(
-        <h1>Hola</h1>
-    )
+"use client";
+
+import { useState } from "react";
+import { MapContainer, TileLayer, useMap, Marker } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+
+export function ChangeView({ coords }) {
+  const map = useMap();
+  map.setView(coords, 6);
+  return null;
+}
+
+
+export default function Geovisor() {
+
+  const [geoData, setGeoData] = useState({ lat: -9.189967, lng: -80.015152 });
+
+  const center = [geoData.lat, geoData.lng];
+  return (
+
+    <MapContainer center={center}  style={{ height: '100vh' }}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <ChangeView coords={center} />
+    </MapContainer>
+
+  );
 }
